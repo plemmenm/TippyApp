63ca75c0d486d954465ed1c79b31ef83949777cc
Patricia Lemmen Meyer <patricialemmenmeyer@patricias-macbook-pro.local>
2018-12-30 17:58:24 -0700

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Patricia Lemmen Meyer on 12/30/18.
+//  Copyright © 2018 Patricia Lemmen Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
